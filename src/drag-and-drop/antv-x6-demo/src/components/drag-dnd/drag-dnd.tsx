dfff5b6be083f013246134f246e6043a80bd92b9
summerUniq <1152972727@qq.com>
2022-04-21 14:51:46 +0800

    feat: template simplied

---
+++ src/drag-and-drop/antv-x6-demo/src/components/drag-dnd/drag-dnd.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Graph, Addon } from '@antv/x6'
+
+export default class DndDrag extends Component {
+    render() {
+        return (
+            <div className="dnd-drag"></div>
+        )
+    }
+}
