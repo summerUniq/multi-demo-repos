a33d28f668e007eb372b4dc1a752454726e0e766
summerUniq <1152972727@qq.com>
2022-04-21 14:23:16 +0800

    save

---
+++ src/drag-and-drop/antv-x6-demo/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import CanvasCore from 'components/canvas-core/canvas-core'
+
+function App() {
+  return (
+    <div className="App">
+      <CanvasCore/>
+    </div>
+  );
+}
+
+export default App;
